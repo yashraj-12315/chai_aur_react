aa81b8041408f959805a89d9e99ad0a698e6e51c
yashraj-12315 <yashraj4586@gmail.com>
2023-09-03 23:54:14 +0530

    committed

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -11,21 +11,24 @@ function App() {
       >
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-            <button onClick={() =>setColor("red")}
+            <button
+              onClick={() => setColor("red")}
               className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-              style={{backgroundColor:"red"}}
+              style={{ backgroundColor: "red" }}
             >
               Red
             </button>
-            <button onClick={() =>setColor("green")}
+            <button
+              onClick={() => setColor("green")}
               className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-              style={{backgroundColor:"green"}}
+              style={{ backgroundColor: "green" }}
             >
               Green
             </button>
-            <button onClick={() =>setColor("blue")}
+            <button
+              onClick={() => setColor("blue")}
               className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-              style={{backgroundColor:"blue"}}
+              style={{ backgroundColor: "blue" }}
             >
               Blue
             </button>
